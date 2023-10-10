9d45d72092297b0b542773bca822648ff31f4932
MHAYA Nizar <nizar.mhaya@gmail.com>
2023-10-10 12:14:04 +0200

    fin matinée

--- SitePro/v2/contact.php
+++ SitePro/v2/contact.php
@@ -1,43 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Bootstrap Version Nizar - contact</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="styles.css" rel="stylesheet" />
-    </head>
-    <body id="page-top">
+<?php
+require_once('template_header.php');
+?>
 
-            <!-- Navigation-->
-            <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-                <div class="container">
-                    <a class="navbar-brand" href="index.html">Nizar Mhaya</a>
-                    <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                        Menu
-                        <i class="fas fa-bars"></i>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarResponsive">
-                        <ul class="navbar-nav ms-auto">
-                            <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="portfolio.html">Portfolio</a></li>
-                            <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="about.html">About</a></li>
-                            <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="contact.html">Contact</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
 
- <!-- Contact Section-->
- <section class="page-section" id="contact">
+<!-- Contact Section-->
+<section class="page-section" id="contact">
     <div class="container">
         <!-- Contact Section Heading-->
         <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contactez-moi</h2>
@@ -99,7 +66,9 @@
                     <!---->
                     <!-- This is what your users will see when there is-->
                     <!-- an error submitting the form-->
-                    <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
+                    <div class="d-none" id="submitErrorMessage">
+                        <div class="text-center text-danger mb-3">Error sending message!</div>
+                    </div>
                     <!-- Submit Button-->
                     <button class="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Send</button>
                 </form>
@@ -107,3 +76,13 @@
         </div>
     </div>
 </section>
+
+
+<!-- Footer-->
+<?php
+require_once('template_footer.php');
+?>
+
+<?php
+require_once('template_copyright.php');
+?>
--- TP2-PHP/excours.php
+++ TP2-PHP/excours.php
@@ -7,19 +7,22 @@ La date d'aujourd'hui est le :
 <?php
 // Affichage de la date
 echo date("d/m/Y");
+echo "<br>" ;
 ?>
 
 <?php echo "Some text"; ?>
 No newline
-<?= "But newline now" ?>
+<?= "<br>But newline now<br>" ?>
 
 <?php
-echo "a"; echo "b"; echo "c";
+echo "a"; echo "b"; echo "c"; echo "<br>" ;
 #The output will be "abc" with no errors
 $a = "Hello ";
 $b = $a . "World !";
 echo $b; //Affiche : "Hello World !"
+echo "<br>" ;
 echo 3*4;
+echo "<br>" ;
 function afficher( $texte, $saut = 1 ) {
     echo $texte;
     for( $i = 0 ; $i < $saut ; $i++)
@@ -28,11 +31,10 @@ function afficher( $texte, $saut = 1 ) {
     afficher ("Yo tout",3);
     afficher ("le monde",2);
     afficher ("c'est Squeezie",1);
+    echo "<br>" ;
     afficher("Hello", 0);
-    afficher(" World !");
-    
+    afficher(" World !"); 
 ?>
-
 
 </body>
 </html>
