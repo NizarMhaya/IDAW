32b97c50d055d718fb1da858c6ced1ce9e64bd65
MHAYA Nizar <nizar.mhaya@gmail.com>
2023-10-11 09:37:52 +0200

    TP3.1 fin

--- TP3/connected.php
+++ TP3/connected.php
@@ -1,32 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connected</title>
-</head>
-
-<body>
-
-    <?php
-    // Vérifie si les données de login et password existent dans la requête GET
-    if (isset($_GET['login']) && isset($_GET['password'])) {
-        // Récupère les valeurs des champs login et password
-        $login = $_GET['login'];
-        $password = $_GET['password'];
-
-        // Affiche les valeurs des champs
-        echo "<h2>Informations de connexion :</h2>";
-        echo "<p><strong>Login :</strong> $login</p>";
-        echo "<p><strong>Mot de passe :</strong> $password</p>";
-    } else {
-        // Si les données ne sont pas présentes, affiche un message d'erreur
-        echo "<h2>Erreur :</h2>";
-        echo "<p>Les données de connexion sont manquantes.</p>";
-    }
-    ?>
-
-</body>
-
-</html>
+<?php
+// on simule une base de données
+$users = array(
+    // login => password
+    'riri' => 'fifi',
+    'yoda' => 'maitrejedi',
+    'Squeezie' => 'yotoutlemonde'
+);
+$login = "anonymous";
+$errorText = "";
+$successfullyLogged = false;
+if (isset($_GET['login']) && isset($_GET['password'])) {
+    $tryLogin = $_GET['login'];
+    $tryPwd = $_GET['password'];
+    // si login existe et password correspond
+    if (array_key_exists($tryLogin, $users) && $users[$tryLogin] == $tryPwd) {
+        $successfullyLogged = true;
+        $login = $tryLogin;
+    } else
+        $errorText = "Erreur de login/password";
+} else
+    $errorText = "Merci d'utiliser le formulaire de login";
+if (!$successfullyLogged) {
+    echo $errorText;
+} else {
+    echo "<h1>Bienvenue " . $login . "</h1>";
+}
