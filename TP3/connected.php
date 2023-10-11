dfbb8ac5d253aa52d0050a0c2ef971d6ae585c45
MHAYA Nizar <nizar.mhaya@gmail.com>
2023-10-11 11:58:15 +0200

    Les sessions ne marchent  pas encore

--- TP3/connected.php
+++ TP3/connected.php
@@ -16,6 +16,9 @@ if (isset($_POST['login']) && isset($_POST['password'])) {
     if (array_key_exists($tryLogin, $users) && $users[$tryLogin] == $tryPwd) {
         $successfullyLogged = true;
         $login = $tryLogin;
+        session_start(); //commencer session ici créer l'objet session et assigner la variable 
+        $_SESSION['username'] = $login;
+        $username = $_SESSION['username'];
     } else
         $errorText = "Erreur de login/password";
 } else
@@ -24,7 +27,29 @@ if (!$successfullyLogged) {
     echo $errorText;
 } else {
     echo "<h1>Bienvenue " . $login . "</h1>";
-    echo "<h2>Votre mot de passe est : " . $tryPwd . "</h2>";
+    echo "<h2>Votre mot de passe est : " . $tryPwd . "</h2>"; // à enlever une fois que session gérera ça
+}
+if (isset($_SESSION['username'])) {
+    echo "<h1>Vous êtes sur la session de $username dans connected </h1>"; // Affiche le nom d'utilisateur sur la page
 }
 
-// Avec get :'URL est : http://localhost/IDAW/TP3/connected.php?login=Squeezie&password=yotoutlemonde
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page connected</title>
+</head>
+
+<body>
+    <p>Cliquez sur le lien ci-dessous pour accéder à une autre page :</p>
+
+    <!-- Lien vers une autre page (par exemple, page2.html) -->
+    <a href="page2.php">Aller à la page 2</a>
+</body>
+
+</html>
---
+++ TP3/page2.php
@@ -0,0 +1,31 @@
+<?php
+session_start(); // Démarre la session (assurez-vous d'appeler session_start() au début de chaque fichier utilisant des sessions)
+
+// Vérifie si l'utilisateur est connecté en vérifiant la session
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username']; // Récupère le nom d'utilisateur depuis la session
+    echo "<h1>Vous êtes sur la session de, $username! dans la page 2</h1>"; // Affiche le nom d'utilisateur sur la page
+} else {
+    // Redirige l'utilisateur vers la page de connexion s'il n'est pas connecté
+    header('Location: login.php');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La page 2</title>
+</head>
+
+<body>
+    <p>Cliquez sur le lien ci-dessous pour accéder à une autre page :</p>
+
+    <!-- Lien vers une autre page (par exemple, page2.html) -->
+    <a href="connected.php">Aller à connected</a>
+</body>
+
+</html>
