ba11fec8d1abad476a93df593f6b97d4971f1fac
MHAYA Nizar <nizar.mhaya@gmail.com>
2023-10-16 03:40:58 +0200

    actual language settings

---
+++ SitePro/v3/en/about.php
@@ -0,0 +1,29 @@
+<!-- About Section-->
+<section class="page-section bg-primary text-white mb-0" id="about">
+    <div class="container">
+        <!-- About Section Heading-->
+        <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+        <!-- Icon Divider-->
+        <div class="divider-custom divider-light">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+            <div class="divider-custom-line"></div>
+        </div>
+        <!-- About Section Content-->
+        <div class="row">
+            <div class="col-lg-4 ms-auto">
+                <p class="lead">Nizar Mhaya is a little genius like few others. Born in the quiet little town of Clamart, Nizar was destined to shine from an early age. His incredible intelligence and insatiable curiosity were obvious to all who knew him. As soon as he started talking, he was asking questions about the world around him, showing an unquenchable thirst for knowledge.</p>
+            </div>
+            <div class="col-lg-4 me-auto">
+                <p class="lead">Growing up, Nizar developed a particular interest in science and technology. He spent hours taking apart and reassembling electronic devices, coding sophisticated computer programs and exploring the mysteries of the universe. His mathematical talents were exceptional. But his imagination surpassed all that, as these two phantasmagorical paragraphs prove.</p>
+            </div>
+        </div>
+        <!-- About Section Button-->
+        <div class="text-center mt-4">
+            <a class="btn btn-xl btn-outline-light" href="https://aoe2recs.com/">
+                <i class="fas fa-download me-2"></i>
+                Best site ever
+            </a>
+        </div>
+    </div>
+</section>
---
+++ SitePro/v3/fr/template_menu.php
@@ -0,0 +1,40 @@
+<?php
+function renderMenuToHTML($currentPageId, $currentLang)
+{
+    // un tableau qui définit la structure du site
+
+    $mymenu = array(
+        // idPage titre
+        'portfolio' => array('Portfolio'),
+        'about' => array('About'),
+        'cv' => array('Cv'),
+        'contact' => array('Contact')
+
+    );
+    echo '<!-- Navigation-->
+        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="index.php?lang=' . $currentLang . '">Nizar Mhaya</a>
+                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">';
+    // ...
+    foreach ($mymenu as $pageId => $pageParameters) {
+        $link = 'index.php?page=' . $pageId . '&lang=' . $currentLang;
+        if ($pageId == $currentPageId) {
+            echo '<li id="currentpage" class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="' . $link . '">' . $pageParameters[0] . '</a></li>';
+        } else {
+            echo '<li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="' . $link . '">' . $pageParameters[0] . '</a></li>';
+        }
+    }
+
+    echo '</ul>
+</div>
+</div>
+</nav>';
+    //}
+    // ...
+}
